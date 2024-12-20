918cdf464580eb3a81a60c1c031405b5b4524d47
felipejbs <felipejeronimobs@gmail.com>
2024-12-20 01:23:34 -0300

    salvando em um banco noSQL

--- src/pipeline_00.py
+++ src/pipeline_00.py
@@ -1,4 +1,6 @@
 import requests
+from tinydb import TinyDB
+from datetime import datetime
 
 def extract_dados_bitcoin():
     url = "https://api.coinbase.com/v2/prices/spot"
@@ -10,16 +12,24 @@ def transform_dados_bitcoin(dados):
     valor = dados["data"]["amount"]
     criptomoeda = dados["data"]["base"]
     moeda = dados["data"]["currency"]
+    timestamp = datetime.now().timestamp()
 
     dados_transformados = {
         "valor": valor,
         "criptomoeda": criptomoeda,
-        "moeda": moeda
+        "moeda": moeda,
+        "timestamp": timestamp
     }
 
     return dados_transformados
 
+def salvar_dados_tinydb(dados, db_name="bitcoin.json"):
+    db = TinyDB(db_name)
+    db.insert(dados)
+    print("Dados salvos com sucesso!")
+
+
 if __name__ == "__main__":
     dados_json = extract_dados_bitcoin()
     dados_tratados = transform_dados_bitcoin(dados_json)
-    print(dados_tratados)
+    salvar_dados_tinydb(dados_tratados)
